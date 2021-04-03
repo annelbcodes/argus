2500dd2d12d40ca2023965145cfc1ae2b67ea8e8
Anne Barrios <commits.noreply@annelb.com>
2021-04-03 22:10:26 +0800

    chore: upgrade deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  purge: [],
+  darkMode: false,
   prefix: '',
   important: false,
   separator: ':',
@@ -196,12 +198,12 @@ module.exports = {
     borderRadius: {
       // none: '0',
       sm: '0.125rem',
-      default: '0.25rem',
+      DEFAULT: '0.25rem',
       // lg: '0.5rem',
       full: '9999px',
     },
     borderWidth: {
-      default: '1px',
+      DEFAULT: '1px',
       // '0': '0',
       // '2': '2px',
       // '4': '4px',
