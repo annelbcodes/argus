8eb0cc0a8556bdbc40b83de4fbf69e86af952529
Anne Barrios <commits.noreply@annelb.com>
2021-05-29 17:12:33 +0800

    feat: added prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,22 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
     env: {
-        "node": true,
-        "es6": true,
+        node: true,
+        es6: true,
     },
     extends: [
-        "eslint:recommended",
-        "plugin:vue/essential",
+        'eslint:recommended',
+        'plugin:vue/essential',
+        'plugin:prettier/recommended',
     ],
     rules: {
-        "no-console": isProd ? "error" : "off"
+        'prettier/prettier': 'error',
+        'no-console': isProd ? 'error' : 'off',
     },
     parserOptions: {
-        "parser": "babel-eslint",
-        "ecmaVersion": 2019,
-        "sourceType": "module"
+        parser: 'babel-eslint',
+        ecmaVersion: '2019',
+        sourceType: 'module',
     },
-    plugins: [
-        "vue",
-    ],
-};
+    plugins: ['vue', 'prettier'],
+}
--- src/components/pages/Main.vue
+++ src/components/pages/Main.vue
@@ -1,171 +1,190 @@
 <template lang="pug">
-
-  .ui(@keydown.ctrl.65="addKey()" tabindex="0")
-
+.ui(@keydown.ctrl.65='addKey()' tabindex='0')
     .ui-body
-      modal(v-if="modal")
-        popup-pref(
-          v-if="ui.type === 'pref'"
-          type="text"
-          action="modify"
-          title="Enter your API key"
-          ctabtn="Save API key"
-          id="pref"
-        )
-        popup-monitor(
-          v-if="ui.type === 'email'"
-          type="email"
-          id="email"
-          action="add"
-          title="Add an email address to monitor"
-          ctabtn="Add to monitor"
-        )
-      .ui-content
-        .sidebar
-        .content
-          title-bar
-            | Argus
-            template(#btn-trigger)
-              monitor-btn(
-                type="email"
-                action="add"
-                text="+"
-              )
-          monitor-list
-      .ui-footer
-        .left
-          svg(
-            v-show="status_checking"
-            height="18"
-            width="18"
-            class="status-icon animate-spin"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="https://www.w3.org/2000/svg"
-          )
-            circle(class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4")
-            path(class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z")
-          | {{ text_timeago }}
-        .right
-          svg(
-            @click.stop.prevent="openPref()"
-            class="pref-icon"
-            height="18"
-            width="18"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          )
-            path(stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z")
-            path(stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z")
-
+        modal(v-if='modal')
+            popup-pref#pref(
+                v-if='ui.type === "pref"'
+                type='text'
+                action='modify'
+                title='Enter your API key'
+                ctabtn='Save API key'
+            )
+            popup-monitor#email(
+                v-if='ui.type === "email"'
+                type='email'
+                action='add'
+                title='Add an email address to monitor'
+                ctabtn='Add to monitor'
+            )
+        .ui-content
+            .sidebar
+            .content
+                title-bar
+                    | Argus
+                    template(#btn-trigger)
+                        monitor-btn(type='email' action='add' text='+')
+                monitor-list
+        .ui-footer
+            .left
+                p
+                    a.cursor-pointer(@click='pauseIntervalChecks()')
+                        svg.h-5.w-5(
+                            xmlns='http://www.w3.org/2000/svg'
+                            viewBox='0 0 20 20'
+                            fill='currentColor'
+                        )
+                            path(
+                                fill-rule='evenodd'
+                                d='M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z'
+                                clip-rule='evenodd'
+                            )
+                svg.status-icon.animate-spin(
+                    v-show='status_checking'
+                    height='18'
+                    width='18'
+                    fill='none'
+                    stroke='currentColor'
+                    viewBox='0 0 24 24'
+                    xmlns='https://www.w3.org/2000/svg'
+                )
+                    circle.opacity-25(
+                        cx='12'
+                        cy='12'
+                        r='10'
+                        stroke='currentColor'
+                        stroke-width='4'
+                    )
+                    path.opacity-75(
+                        fill='currentColor'
+                        d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+                    )
+                | {{ text_timeago }}
+            .right
+                svg.pref-icon(
+                    @click.stop.prevent='openPref()'
+                    height='18'
+                    width='18'
+                    fill='none'
+                    stroke='currentColor'
+                    viewBox='0 0 24 24'
+                    xmlns='http://www.w3.org/2000/svg'
+                )
+                    path(
+                        stroke-linecap='round'
+                        stroke-linejoin='round'
+                        stroke-width='2'
+                        d='M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z'
+                    )
+                    path(
+                        stroke-linecap='round'
+                        stroke-linejoin='round'
+                        stroke-width='2'
+                        d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'
+                    )
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex'
-import { mType }    from '@/store/mutationtypes'
-import TitleBar     from '@/components/widgets/TitleBar'
-import Modal        from '@/components/widgets/Modal'
-import MonitorList  from '@/components/widgets/MonitorList'
-import MonitorBtn   from '@/components/widgets/MonitorBtn'
+import { mType } from '@/store/mutationtypes'
+import TitleBar from '@/components/widgets/TitleBar'
+import Modal from '@/components/widgets/Modal'
+import MonitorList from '@/components/widgets/MonitorList'
+import MonitorBtn from '@/components/widgets/MonitorBtn'
 import PopupMonitor from '@/components/widgets/PopupMonitor'
-import PopupPref    from '@/components/widgets/PopupPreferences'
-import moment       from 'moment'
+import PopupPref from '@/components/widgets/PopupPreferences'
+import moment from 'moment'
 
 export default {
-  components: {
-    TitleBar,
-    MonitorList,
-    MonitorBtn,
-    Modal,
-    PopupMonitor,
-    PopupPref,
-  },
-  data() {
-    return {
-      text_timeago: '',
-      status_checking: false,
-      obj_action: {
-        action: '',
-        type: ''
-      }
-    }
-  },
-  mounted() {
-    this.setInterval()
-    window.ipcRenderer.send('REQ_API_STORE')
-  },
-  computed: {
-    ...mapState({
-      modal: state => state.ui.modal
-    }),
-    ...mapState([
-      'cd',
-      'ui',
-    ]),
-  },
-  watch: {
-    cd: {
-      deep: true,
-      handler() {
+    components: {
+        TitleBar,
+        MonitorList,
+        MonitorBtn,
+        Modal,
+        PopupMonitor,
+        PopupPref,
+    },
+    data() {
+        return {
+            text_timeago: '',
+            status_checking: false,
+            obj_action: {
+                action: '',
+                type: '',
+            },
+        }
+    },
+    mounted() {
         this.setInterval()
-      }
+        window.ipcRenderer.send('REQ_API_STORE')
     },
-  },
-  methods: {
-    ...mapActions({
-      toggleModal: mType.MODAL_TOGGLE
-    }),
-
-    setTimeAgo() {
-      moment.updateLocale('en', {
-          relativeTime : {
-            ss: this.cd.cdi +"s"
-          }
-      })
-      // moment.relativeTimeThreshold('ss', 3)
-      let timeago = this.cd.interval - this.cd.cdi
-
-      switch (true) {
-        case (this.cd.interval < 60):
-          this.text_timeago = timeago +'s ago'
-          break
-        case (this.cd.interval >= 60):
-          this.text_timeago = moment.duration(-this.cd.interval, 'seconds').humanize(true)
-          break
-      }
-
+    computed: {
+        ...mapState({
+            modal: (state) => state.ui.modal,
+        }),
+        ...mapState(['cd', 'ui']),
     },
-
-    setInterval() {
-      if (this.cd.cdi === this.cd.interval) {
-        this.text_timeago = ''
-        this.status_checking = true
-      }
-      else {
-        this.setTimeAgo()
-        this.status_checking = false
-      }
+    watch: {
+        cd: {
+            deep: true,
+            handler() {
+                this.setInterval()
+            },
+        },
     },
+    methods: {
+        ...mapActions({
+            toggleModal: mType.MODAL_TOGGLE,
+        }),
 
-    openPref() {
-      this.obj_action = {
-        action: 'add',
-        type: 'pref',
-      }
-      this.toggleModal(this.obj_action)
-    },
+        setTimeAgo() {
+            moment.updateLocale('en', {
+                relativeTime: {
+                    ss: this.cd.cdi + 's',
+                },
+            })
+            // moment.relativeTimeThreshold('ss', 3)
+            let timeago = this.cd.interval - this.cd.cdi
 
-    addKey() {
-      this.obj_action = {
-        action: 'add',
-        type: 'email'
-      }
-      this.toggleModal(this.obj_action)
+            switch (true) {
+                case this.cd.interval < 60:
+                    this.text_timeago = timeago + 's ago'
+                    break
+                case this.cd.interval >= 60:
+                    this.text_timeago = moment
+                        .duration(-this.cd.interval, 'seconds')
+                        .humanize(true)
+                    break
+            }
+        },
+
+        setInterval() {
+            if (this.cd.cdi === this.cd.interval) {
+                this.text_timeago = ''
+                this.status_checking = true
+            } else {
+                this.setTimeAgo()
+                this.status_checking = false
+            }
+        },
+
+        pauseIntervalChecks() {},
+
+        openPref() {
+            this.obj_action = {
+                action: 'add',
+                type: 'pref',
+            }
+            this.toggleModal(this.obj_action)
+        },
+
+        addKey() {
+            this.obj_action = {
+                action: 'add',
+                type: 'email',
+            }
+            this.toggleModal(this.obj_action)
+        },
     },
-  },
 }
 </script>
 
